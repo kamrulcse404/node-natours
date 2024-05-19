4f16b694839acc0939382a4c58e46e9a85e2d1f5
kamrulcse404 <kamrul.h01960@gmail.com>
2024-05-19 21:16:19 +0600

    chaining multiple middleware functiuon

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,7 @@ let tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-exports.checkID = (req, res, next, val) =>{
+exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is ${val}`);
   const id = req.params.id * 1;
 
@@ -14,8 +14,18 @@ exports.checkID = (req, res, next, val) =>{
       message: 'invalid id',
     });
   }
-next();
-}
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -30,7 +40,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
-
 
   res.status(200).json({
     status: 'success',
@@ -67,8 +76,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -78,11 +85,8 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
- 
   res.status(204).json({
     status: 'success',
     data: null,
   });
 };
-
-
