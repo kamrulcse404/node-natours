31ce955bac79f4151789b9dc97ba17bfdf236dab
kamrulcse404 <kamrul.h01960@gmail.com>
2024-05-19 10:58:58 +0600

    express middleware

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const fs = require('fs');
-
 const app = express();
+
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
