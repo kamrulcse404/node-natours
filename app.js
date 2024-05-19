bc77c4055caf7ff2b90225fd498c8bc59529c722
kamrulcse404 <kamrul.h01960@gmail.com>
2024-05-19 20:57:36 +0600

    param middleware

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   console.log('hello from the middleware 😁');
-//   next();
-// });
+app.use((req, res, next) => {
+  console.log('hello from the middleware 😁');
+  next();
+});
 
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString();
-//   next();
-// });
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,19 @@ let tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) =>{
+  console.log(`Tour id is ${val}`);
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      staus: 'fail',
+      message: 'invalid id',
+    });
+  }
+next();
+}
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -18,12 +31,6 @@ exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
-  if (!tour) {
-    return res.status(404).json({
-      staus: 'fail',
-      message: 'invalid id',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
@@ -60,14 +67,7 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
 
-  if (id > tours.length) {
-    return res.status(404).json({
-      staus: 'fail',
-      message: 'invalid id',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
@@ -78,15 +78,7 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      staus: 'fail',
-      message: 'invalid id',
-    });
-  }
-
+ 
   res.status(204).json({
     status: 'success',
     data: null,
